e31858f65d930ae4363d7e8fea80224674fbc4df
grokmann <grokmann@gmail.com>
2019-12-07 23:20:11 -0500

    Cleaned up code. Removed comments.

--- day-1.rkt
+++ day-1.rkt
@@ -1,13 +1,6 @@
 #lang racket
-(require net/http-client)
-(require net/url)
-;https://adventofcode.com/2019/day/1/input
-;(call/input-url (string->url "https://adventofcode.com/2019/day/1/input")
-;                get-pure-port
-;                port->string)
 (define data (file->list "day-1-input.txt"))
 (foldl (lambda (a result)
-         ;(printf "~a ~v ~%" result a)
          (+ result (- (floor (/ a 3)) 2)))
        0
        data)
